a55dc308dbeee937c3349393c08fd8aa990c2779
unknown <samuel.goy@insa-lyon.fr>
2019-05-01 21:04:28 +0200

    map

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -8,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class MapComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit() {
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myfrugalmap = L.map('mymap').setView([45.7537, 4.8630], 13);
+   
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    }).addTo(myfrugalmap);
   }
-
-}
+  }
