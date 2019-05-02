06b517f46fc8a02e801d9a61489fcefc3dfcd0e3
unknown <samuel.goy@insa-lyon.fr>
2019-05-02 09:14:07 +0200

    CHangement de map

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -14,20 +14,26 @@ export class MapComponent implements OnInit {
     const mymap = L.map('mymap').setView([45.7537, 4.8630], 13);
 
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    }).addTo(mymap);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mymap);
 
     
     
-    var circle = L.circle([45.774326, 4.868297], {
+  var circle = L.circle([45.774326, 4.868297], {
       color: 'red',
       fillColor: '#f03',
       fillOpacity: 0.5,
-      radius: 200
+      radius: 20
   }).addTo(mymap);
 
-  circle.bindPopup("Lio est le + bô");
+  circle.bindPopup("Place Witson");
+
+  circle.on('click', function(){
+    console.log("test");
+  })
   
+
   
 
   }
