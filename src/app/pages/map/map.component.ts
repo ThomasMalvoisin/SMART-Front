453018c36ec363714969437247224d7d8a5f586c
unknown <samuel.goy@insa-lyon.fr>
2019-05-03 15:47:09 +0200

    menu droite

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -14,7 +14,13 @@ export class MapComponent implements OnInit {
       private busStops = {
             bus_stops: []
       };
-      
+      //ArretActif
+      private nomArretActif: String;
+      latitudeActif: number;
+      longitudeActif: number;
+      personneALArret: number;
+      personneArrivant: number;
+
       busX: number[]; //Absicce des bus
       busY: number[]; //Ordonnée des bus
       personneBus: number[]; //nombre de personne dans le bus
@@ -26,6 +32,9 @@ export class MapComponent implements OnInit {
       constructor(
             private busStopService : BusStopService
       ) {
+            
+
+
             this.infoBusStop=new Array()
             var i=0;
             for(i;i<100;i++){
@@ -35,7 +44,7 @@ export class MapComponent implements OnInit {
             this.busArret =new Array();
 
             this.circleBusStop = new Array();
-            this.retrieveAllBusStops();  
+             
             
             
 
@@ -48,10 +57,17 @@ export class MapComponent implements OnInit {
 
       }
       ngOnInit() {
-            this.mymap = L.map('mymap').setView([45.7537, 4.8630], 13);           
+            this.nomArretActif = '1';
+            this.latitudeActif = 1;
+            this.longitudeActif = 1;
+            this.personneALArret= 5;
+            this.personneArrivant= 1;
+            this.retrieveAllBusStops(); 
+            this.mymap = L.map('mymap').setView([45.769448, 4.861025], 15);           
             L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(this.mymap);
+            
       }
 
       retrieveAllBusStops()
@@ -62,42 +78,34 @@ export class MapComponent implements OnInit {
                   var i =0
                   this.busStops.bus_stops.forEach(busStop => 
                   {
-                  this.infoBusStop[i][0]=busStop.name;
-                              this.infoBusStop[i][1]=getRandomInt(60); //nombre de personne a larret
-                              this.infoBusStop[i][2]='8h34';
-                              this.infoBusStop[i][3]='9';
-                              this.infoBusStop[i][4]='10h04';
-                              this.infoBusStop[i][5]='6';
-                              this.infoBusStop[i][6]='14h24';
-                              this.infoBusStop[i][7]='11';
-                              this.infoBusStop[i][8]='15h29';
-                              this.infoBusStop[i][9]='2';
-                              this.infoBusStop[i][10]=-1;//pour la fin du tableau
-                             
-
-                              this.circleBusStop[i] = L.circle([busStop.longitude, busStop.latitude] , {
+                              this.circleBusStop[i] = L.circle([busStop.latitude, busStop.longitude] , {
                                     color: 'red',
                                     fillColor: '#f03',
-                                    fillOpacity: 0.5,
-                                    radius: this.infoBusStop[i][1]
+                                    fillOpacity: 0.2,
+                                    radius: busStop.nbPersonsWaiting+1
                               }).addTo(this.mymap);
                               this.circleBusStop[i].bindPopup(busStop.name);
                               
 
-                              this.circleBusStop[i].on('click', busStopDetails(i]));
-                              i++;
+                              this.circleBusStop[i].on('click', this.onBusStopSelected.bind(this, busStop));
                               
-                  }
+                  })
                   console.log(res);
             }, err => 
             {
                   console.log(err);
             });
       }
-      function getRandomInt(max) {
-            return Math.floor(Math.random() * Math.floor(max));
+
+      onBusStopSelected(busStop){
+            console.log(busStop);
+            this.nomArretActif= busStop.name;
+            this.latitudeActif= busStop.latitude;
+            this.longitudeActif= busStop.longitude;
+            this.personneALArret= busStop.nbPersonsWaiting;
+            this.personneArrivant= busStop.nbPersonsComing;
       }
-      function busStopDetails(a) {
-            
+      getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
       }
 }
