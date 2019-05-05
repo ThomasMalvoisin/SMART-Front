afc11cde422b48cc194263d6504361f40305c59e
unknown <samuel.goy@insa-lyon.fr>
2019-05-05 22:26:39 +0200

    formulaire demo : busStop

--- src/app/pages/demo/demo.component.ts
+++ src/app/pages/demo/demo.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { BusStopService } from 'src/app/services/bus-stop.service';
 
 @Component({
   selector: 'app-demo',
@@ -6,48 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent implements OnInit {
+  private busStops = {
+    bus_stops: []
+  };
+  @ViewChild('one') d1:ElementRef;
 
-  constructor() { }
+  constructor(  @Inject(DOCUMENT) private document: any,
+  private busStopService: BusStopService,
+  private renderer:Renderer2) { }
 
   ngOnInit() {
-  // var ctx = document.getElementById("myChart").getContext('2d');
-  // var myChart = new Chart(ctx, {
-  //   type: 'bar',
-  //   data: {
-  //     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  //     datasets: [{
-  //       label: '# of Votes',
-  //       data: [12, 19, 3, 5, 2, 3],
-  //       backgroundColor: [
-  //         'rgba(255, 99, 132, 0.2)',
-  //         'rgba(54, 162, 235, 0.2)',
-  //         'rgba(255, 206, 86, 0.2)',
-  //         'rgba(75, 192, 192, 0.2)',
-  //         'rgba(153, 102, 255, 0.2)',
-  //         'rgba(255, 159, 64, 0.2)'
-  //       ],
-  //       borderColor: [
-  //         'rgba(255,99,132,1)',
-  //         'rgba(54, 162, 235, 1)',
-  //         'rgba(255, 206, 86, 1)',
-  //         'rgba(75, 192, 192, 1)',
-  //         'rgba(153, 102, 255, 1)',
-  //         'rgba(255, 159, 64, 1)'
-  //       ],
-  //       borderWidth: 1
-  //     }]
-  //   },
-  //   options: {
-  //     scales: {
-  //       yAxes: [{
-  //         ticks: {
-  //           beginAtZero: true
-  //         }
-  //       }]
-  //     }
-  //   }
-  // });
-
+    this.retrieveAllBusStops();
+    
   }
+
+  addBusStopsToDetails() { // Cette fonction là ne marche pas, je comprends pas pourquoi même avec un truc tout simple
+    var i =1;
+    this.busStops.bus_stops.forEach(busStop => {
+      var busStopNum= "busStop" +i;
+      console.log(busStopNum);
+      var div = this.renderer.createElement('div');
+      var label = this.renderer.createElement('label');
+      this.renderer.setAttribute(label, 'for', busStop.name);
+      var text = this.renderer.createText(busStop.name + " : "+" \u00a0");
+      this.renderer.appendChild(label, text);
+      this.renderer.appendChild(div, label);
+      var input = this.renderer.createElement('input');
+      this.renderer.setAttribute(input, 'type' , 'number');
+      this.renderer.appendChild(div, input);
+      this.renderer.appendChild(this.d1.nativeElement, div);
+      i++;
+      });
+      
+  }
+
+  retrieveAllBusStops() {
+    this.busStopService.retrieveAll().subscribe((res: any) => {
+          this.busStops = res;
+          console.log(this.busStops);
+          this.addBusStopsToDetails();
+    }, err => {
+          console.log(err);
+    });
+}
 
 }
